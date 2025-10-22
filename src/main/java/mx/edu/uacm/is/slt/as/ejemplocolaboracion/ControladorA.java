0e8572cc80a26fe0b7ba2f246ade2c53eea38044
FrancisF-x <francisco.acosta@alumnos.uacm.edu.mx>
2025-10-22 17:05:18 -0600

    Creado controlador A

---
+++ src/main/java/mx/edu/uacm/is/slt/as/ejemplocolaboracion/ControladorA.java
@@ -0,0 +1,14 @@
+package mx.edu.uacm.is.slt.as.ejemplocolaboracion;
+
+/**
+ *
+ * @author Frncs.Fox
+ */
+public class ControladorA implements PanelControl {
+
+    @Override
+    public void evento() {
+        System.out.println("Controlador A");
+    }
+    
+}
